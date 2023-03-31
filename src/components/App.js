a13a4301a0b36f948944b9f214931c847d0f627a
insomniac2305 <84805847+insomniac2305@users.noreply.github.com>
2023-03-31 17:36:08 +0200

    Implement addToCard and removeFromCart functions

--- src/components/App.js
+++ src/components/App.js
@@ -4,15 +4,48 @@ import Home from "./Home";
 import Item from "./Item";
 import Shop from "./Shop";
 import itemData from "../data/items.json";
+import { useState } from "react";
 
 function App() {
+  const [cartItems, setCartItems] = useState([]);
+
+  const addToCart = (itemId, count) => {
+    const cartItemIndex = cartItems.findIndex((it) => it.id === itemId);
+    if (cartItemIndex === -1) {
+      const newItem = {
+        id: itemId,
+        count: count,
+      };
+      setCartItems([...cartItems, newItem]);
+    } else {
+      const newCartItems = [...cartItems];
+      newCartItems[cartItemIndex].count += count;
+      setCartItems(newCartItems);
+    }
+  };
+
+  const removeFromCart = (itemId, count) => {
+    const cartItemIndex = cartItems.findIndex((it) => it.id === itemId);
+    if (cartItemIndex === -1) return;
+
+    const newCartItems = [...cartItems];
+
+    if (newCartItems[cartItemIndex].count - count <= 0) {
+      newCartItems.splice(cartItemIndex, 1);
+    } else {
+      newCartItems[cartItemIndex].count -= count;
+    }
+
+    setCartItems(newCartItems);
+  };
+
   return (
     <BrowserRouter>
       <Routes>
         <Route path="/" element={<Home />} />
-        <Route path="/shop" element={<Shop items={itemData} />} />
-        <Route path="/cart" element={<Cart items={itemData}/>} />
-        <Route path="/shop/:itemId" element={<Item />} />
+        <Route path="/shop" element={<Shop items={itemData} onAdd={addToCart} />} />
+        <Route path="/cart" element={<Cart items={cartItems} onAdd={addToCart} onRemove={removeFromCart} />} />
+        <Route path="/shop/:itemId" element={<Item onAdd={addToCart} />} />
       </Routes>
     </BrowserRouter>
   );
