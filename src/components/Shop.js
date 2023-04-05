069b91fc4482aba5c66d4722a1c2cd5b3952f46e
insomniac2305 <84805847+insomniac2305@users.noreply.github.com>
2023-04-05 22:03:01 +0200

    Design Shop page

--- src/components/Shop.js
+++ src/components/Shop.js
@@ -4,15 +4,18 @@ function Shop({ items, onAdd }) {
   const itemList = items.map((item) => {
     return (
       <li key={item.id}>
-        <ShopItem id={item.id} name={item.name} onAdd={onAdd}/>
+        <ShopItem id={item.id} name={item.name} image={item.images[0]} onAdd={onAdd} />
       </li>
     );
   });
 
   return (
-    <div>
-      <h1>Shop</h1>
-      <ul>{itemList}</ul>
+    <div className="bg-emerald-50 dark:bg-gray-900 flex justify-center">
+      <ul
+        className="grid grid-cols-[repeat(auto-fit,_minmax(15rem,_1fr))] auto-rows-[21rem] gap-y-4 w-full max-w-screen-xl"
+      >
+        {itemList}
+      </ul>
     </div>
   );
 }
