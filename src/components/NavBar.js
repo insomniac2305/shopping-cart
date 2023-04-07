2834b9630a8230fe26f380e9fc6cf7b0ca408368
insomniac2305 <84805847+insomniac2305@users.noreply.github.com>
2023-04-07 13:08:37 +0200

    Add cart count and cart icon

--- src/components/NavBar.js
+++ src/components/NavBar.js
@@ -1,6 +1,7 @@
+import { FaShoppingCart } from "react-icons/fa";
 import { Link } from "react-router-dom";
 
-function NavBar() {
+function NavBar({cartCount}) {
   return (
     <header className="shadow-md bg-slate-950">
       <nav className="mx-auto flex max-w-screen-2xl flex-wrap items-center justify-between p-4">
@@ -18,9 +19,10 @@ function NavBar() {
         </Link>
         <Link
           to="/cart"
-          className="text-2xl font-bold text-white transition-all hover:scale-110 hover:text-violet-600 "
+          className="relative text-2xl font-bold text-white transition-all hover:scale-110 hover:text-violet-600 "
         >
-          Cart
+          <span className="absolute -right-2 -top-2 w-4 h-4 text-center leading-none text-sm bg-emerald-300 text-emerald-900 rounded-full">{cartCount}</span>
+          <FaShoppingCart />
         </Link>
       </nav>
     </header>
